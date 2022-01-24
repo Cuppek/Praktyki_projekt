aa94dd6ea0acd6ee1e792c9c4d09274d807420bc
Milosz_Cuppa <milosz.cuppa@gmail.com>
2022-01-24 13:14:06 +0100

    Załadowane pliki bootstrapa i przetestowane na prostej stronie

--- public/index.php
+++ public/index.php
@@ -1,9 +1,24 @@
 <?php
+//
+//use App\Kernel;
+//
+//require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+//
+//return function (array $context) {
+//    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+//};
 
-use App\Kernel;
+?>
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+<html>
+    <head>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    </head>
+    <body>
+        <button type="button" class="btn btn-success">Primary</button>
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+        <script src="assets/js/jquery-3.6.0.slim.min.js"></script>
+        <script src="assets/js/popper.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+    </body>
+</html>
