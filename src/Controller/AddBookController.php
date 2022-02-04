46da994eef60a30bb0ce6848e14e784d9f3579ec
Milosz_Cuppa <milosz.cuppa@gmail.com>
2022-02-04 19:01:41 +0100

    Dodano zabezpieczenie przed nieautoryzowanym dostępem do kontrolera dodawania książek

--- src/Controller/AddBookController.php
+++ src/Controller/AddBookController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Form\AddBookType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted("ROLE_ADMIN")]
 class AddBookController extends AbstractController
 {
     #[Route('/add_book', name: 'add_book')]
